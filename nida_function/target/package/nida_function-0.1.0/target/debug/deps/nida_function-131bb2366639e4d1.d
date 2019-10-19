ea25e6113a4c58e0451ab36d024446c9031c1268
Nida Khan <knida5637@gmail.com>
2019-10-19 17:31:52 +0000

    my first crate library commit

---
+++ nida_function/target/package/nida_function-0.1.0/target/debug/deps/nida_function-131bb2366639e4d1.d
@@ -0,0 +1,7 @@
+/home/nida/Desktop/iot_nida/crates_library/nida_function/target/package/nida_function-0.1.0/target/debug/deps/nida_function-131bb2366639e4d1.rmeta: src/lib.rs
+
+/home/nida/Desktop/iot_nida/crates_library/nida_function/target/package/nida_function-0.1.0/target/debug/deps/libnida_function-131bb2366639e4d1.rlib: src/lib.rs
+
+/home/nida/Desktop/iot_nida/crates_library/nida_function/target/package/nida_function-0.1.0/target/debug/deps/nida_function-131bb2366639e4d1.d: src/lib.rs
+
+src/lib.rs:
